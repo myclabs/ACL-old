69c2f9bb02fdb8d7fe342a1d3a6d5df0cf6cc9d0
Matthieu Napoli <matthieu@mnapoli.fr>
2013-02-24 18:34:48 +0100

    Full action list

--- src/Mycsense/ACL/Action.php
+++ src/Mycsense/ACL/Action.php
@@ -9,15 +9,48 @@ class Action extends Enum
 
     const VIEW = 'view';
     const EDIT = 'edit';
+    const CREATE = 'create';
+    const DELETE = 'delete';
+    const UNDELETE = 'undelete';
 
+    /**
+     * @return Action
+     */
     public static function VIEW()
     {
         return new Action(self::VIEW);
     }
 
+    /**
+     * @return Action
+     */
     public static function EDIT()
     {
         return new Action(self::EDIT);
+    }
+
+    /**
+     * @return Action
+     */
+    public static function CREATE()
+    {
+        return new Action(self::CREATE);
+    }
+
+    /**
+     * @return Action
+     */
+    public static function DELETE()
+    {
+        return new Action(self::DELETE);
+    }
+
+    /**
+     * @return Action
+     */
+    public static function UNDELETE()
+    {
+        return new Action(self::UNDELETE);
     }
 
 }
